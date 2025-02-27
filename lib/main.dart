a8f734a16042408c9856164e3424be0179f05c48
Aswajith <aswajith.3747@gmail.com>
2025-02-27 20:11:09 +0530

    playing with row, column, expanded, flex, alignment

--- lib/main.dart
+++ lib/main.dart
@@ -28,16 +28,84 @@ class HomeScreen extends StatelessWidget {
       backgroundColor: const Color.fromARGB(255, 239, 174, 77),
       appBar: AppBar(),
       body: SafeArea(
-        child: Center(
-          child: Text(
-            text,
-            textAlign: TextAlign.center,
-            style: const TextStyle(
-              color: Colors.yellow,
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(child: Container(color: Colors.red)),
+            Expanded(
+              flex: 2,
+              child: Container(
+                color: Colors.yellow,
+                child: Column(
+                  children: [
+                    Text(
+                      text,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        color: Colors.yellow,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            print("TextButton clicked");
+                          },
+                          child: Text('Click Me'),
+                        ),
+                        IconButton(onPressed: () {}, icon: Icon(Icons.info)),
+                      ],
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        print('Elevated button clicked');
+                      },
+                      child: Text("Click me 2nd"),
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ),
+            Expanded(
+              child: Container(
+                color: Colors.orange,
+                child: Column(
+                  children: [
+                    Text(
+                      text,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        color: Colors.yellow,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            print("TextButton clicked");
+                          },
+                          child: Text('Click Me'),
+                        ),
+                        IconButton(onPressed: () {}, icon: Icon(Icons.info)),
+                      ],
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        print('Elevated button clicked');
+                      },
+                      child: Text("Click me 2nd"),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
